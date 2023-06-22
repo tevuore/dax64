bda363f8709a956e990a66e6623b71808230ec48
Tero Vuorela <tero.vuorela@iki.fi>
2023-06-22 11:05:33 +0300

    Setup dissamle command

--- bin/c64.dart
+++ bin/c64.dart
@@ -1,11 +1,11 @@
 import 'package:args/command_runner.dart';
-import 'package:c64/commit_command.dart';
+import 'package:c64/disassemble_command.dart';
 import 'package:c64/parse_basic_data_command.dart';
 
 Future<int> main(List<String> args) async {
   final runner = CommandRunner("c64", "Commodore 64 machine code utilities")
-    ..addCommand(CommitCommand())
     ..addCommand(ParseBasicDataCommand())
+    ..addCommand(DisassembleCommand())
     ..argParser.addFlag('verbose', abbr: 'v', help: 'increase logging');
 
   final result = await runner.run(args);
