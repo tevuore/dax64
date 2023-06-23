1a82cc42292faa9e00588220beaf3d9c2008e275
Tero Vuorela <tero.vuorela@iki.fi>
2023-06-23 09:50:46 +0300

    Add formatter for hex output

--- lib/hex_formatter.dart
+++ lib/hex_formatter.dart
@@ -1,11 +1,31 @@
 import 'dart:typed_data';
 
+import 'package:c64/models/instruction.dart';
 import 'package:c64/parse_basic_data_command.dart';
+import 'package:c64/utils/hex8bit.dart';
 
 class HexFormatter {
   static String format(Uint8List bytes) {
     List<String> hexCodes = bytes.map((number) => uint8ToHex(number)).toList();
 
     return hexCodes.join(' ');
+  }
+
+  static Uint8List formatToBytes(Program program) {
+    final buffer = Uint8List(program.instructions.length * 3);
+    
+    for (var instruction in program.instructions) {
+      buffer.add(parse8BitHex(instruction.opcode.opcode));
+      if (instruction.paramBytes.isNotEmpty) {
+        if (instruction.paramBytes.length == 1) {
+          buffer.add(instruction.paramBytes[0]);
+        }
+        if (instruction.paramBytes.length > 1) {
+          buffer.add(instruction.paramBytes[1]);
+          buffer.add(instruction.paramBytes[0]);
+        }
+      }
+    }
+    return buffer;
   }
 }
