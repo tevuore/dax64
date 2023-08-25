a243e113b63e022f9f12bc2a83639cf6c20a395d
Tero Vuorela <tero.vuorela@iki.fi>
2023-08-25 08:28:14 +0300

    Add check for test execution

--- test_execution_flow.sh
+++ test_execution_flow.sh
@@ -10,16 +10,23 @@ echo "Use /output dir for outputs"
 mkdir -p output
 
 echo "----------"
-dart run bin/dax64.dart parsebasicdata --input-file programs/program.bas --output-file output/program.bin
+BIN_FILE=output/program.bin
+dart run bin/dax64.dart parsebasicdata --input-file programs/program.bas --output-file $BIN_FILE
+if [ ! -s "${BIN_FILE}" ]; then
+    echo "ERROR: $BIN_FILE file is empty"
+    exit 1
+fi
 
 echo "----------"
 dart run bin/dax64.dart disassemble --input-file output/program.bin --output-file output/program.asm --add-instruction-description
+cat output/program.asm
 
 echo "----------"
 dart run bin/dax64.dart assemble --input-file output/program.asm --output-file output/program.bin
 
 echo "----------"
 dart run bin/dax64.dart basicloader --input-file output/program.bin --output-file output/program_2.bas
+cat output/program_2.bas
 
 echo "----------"
 ./basicload.sh output/program_2.bas
