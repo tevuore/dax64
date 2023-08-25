a2757256ef2dfa542c6be58f7fa09c76a081d734
Tero Vuorela <tero.vuorela@iki.fi>
2023-08-25 08:16:57 +0300

    Use exit() in main()
    Eventhough you can specify return value, dart won't use that as exit
    code.

--- bin/dax64.dart
+++ bin/dax64.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:args/command_runner.dart';
 import 'package:dax64/command/assemble_command.dart';
 import 'package:dax64/command/basic_loader_command.dart';
 import 'package:dax64/command/disassemble_command.dart';
 import 'package:dax64/command/parse_basic_data_command.dart';
 
-Future<int> main(List<String> args) async {
+Future<void> main(List<String> args) async {
   final runner = CommandRunner("dax64", "Commodore 64 machine code utilities")
     ..addCommand(ParseBasicDataCommand())
     ..addCommand(DisassembleCommand())
@@ -12,8 +14,15 @@ Future<int> main(List<String> args) async {
     ..addCommand(BasicLoaderCommand())
     ..argParser.addFlag('verbose', abbr: 'v', help: 'increase logging');
 
-  final result = await runner.run(args);
+  try {
+    final result = await runner.run(args);
 
-  if (result is int) return result;
-  return 0;
+    if (result is int) exit(result);
+    exitCode = 0;
+  } catch (e, stacktrace) {
+    // TODO with verbose flag print stacktrace
+    print(e);
+    print(stacktrace);
+    exitCode = 1;
+  }
 }
